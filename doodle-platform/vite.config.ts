ccb781fed5ff702755d2f800c4a888be5fa04a16
Melyn Were <WereMA@cardiff.ac.uk>
2025-06-24 22:28:50 +0100

    rename index css file to main

--- doodle-platform/vite.config.ts
+++ doodle-platform/vite.config.ts
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    open: true
+     watch: {
+      usePolling: true, // try enabling this on Windows/WSL
+    },
+    hmr: true,
   }
 })
