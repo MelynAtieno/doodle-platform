91a38849dc993207364477e18d4cbbc086a5bda0
Melyn Were <WereMA@cardiff.ac.uk>
2025-06-24 23:56:03 +0100

    add mouse movement functions

--- doodle-platform/src/CanvasComponent.tsx
+++ doodle-platform/src/CanvasComponent.tsx
@@ -1,7 +1,10 @@
-import { useEffect, useRef } from "react"
+import { useEffect, useRef, useState } from "react"
 
 const CanvasComponent = () => {
     const canvasRef = useRef<HTMLCanvasElement | null>(null);
+    const [isDrawing, setIsDrawing] = useState(false);
+    const [canvasContext, setCanvasContext] = useState<CanvasRenderingContext2D | null>(null);
+
 
     useEffect(() => {
         const canvas = canvasRef.current;
@@ -15,11 +18,18 @@ const CanvasComponent = () => {
                 context.lineCap = "round";
                 context.strokeStyle = "black";
                 context.lineWidth = 5;
+                setCanvasContext(context)
             }
         } else {
             console.log("waiting...")
         }
     }, []);
+    
+    const startDrawing = () =>{};
+
+    const stopDrawing = () =>{};
+
+    const draw = () =>{};
     
 
     return (<div style={{
@@ -27,7 +37,8 @@ const CanvasComponent = () => {
         justifyContent: "center",
         padding: "20px"
     }}>
-        <canvas ref={canvasRef} id="doodleCanvas" style={{ 
+        <canvas ref={canvasRef} id="doodleCanvas" onMouseDown={startDrawing} onMouseMove={draw} onMouseUp={stopDrawing} onMouseLeave={stopDrawing}
+        style={{ 
             border: "2px solid", 
             borderRadius:"8px", 
             backgroundColor: "white", 
